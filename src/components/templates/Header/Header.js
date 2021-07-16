eea6f1f9d41c274dd5d17cf83cb8150488ea6421
Connor Mackay <connor.mackay@gmail.com>
2021-07-16 17:44:54 -0400

    Add logo to header

--- src/components/templates/Header/Header.js
+++ src/components/templates/Header/Header.js
@@ -5,7 +5,7 @@ import React from 'react';
 
 /* Asset Imports */
 import StyledHeader from './StyledHeader';
-import logo from 'https://icon2.cleanpng.com/20180425/xrw/kisspng-logo-brand-communication-design-ornamentation-5ae0e069496334.8998592215246869533006.jpg'
+import logo from 'src/assets/images/logo.png'
 
 const Header = () => {
   return (
--- src/components/templates/Header/StyledHeader.js
+++ src/components/templates/Header/StyledHeader.js
@@ -1,7 +1,13 @@
 import styled from 'styled-components';
 
 const StyledHeader = styled.section`
-
+background-color: #6E0A70;
+width: 100%;
+height: 100px;
+img{
+  display: block;
+  max-height: 100%;
+}
 `
 
 export default StyledHeader;
