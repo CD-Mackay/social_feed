a1fc6c8f3eefc2ce3f4cd316190bf3b12136c112
Connor Mackay <connor.mackay@gmail.com>
2021-07-16 18:01:01 -0400

    Style navigation

--- src/components/templates/Header/StyledHeader.js
+++ src/components/templates/Header/StyledHeader.js
@@ -3,10 +3,11 @@ import styled from 'styled-components';
 const StyledHeader = styled.section`
   background-color: #6E0A70;
   display: grid;
-  grid-template-columns: 230px 500px;
+  grid-template-columns: 230px 300px;
   justify-content: space-between;
   height: 100px;
   width: 100%;
+  align-items: center;
   
   img{
     display: block;
@@ -17,6 +18,10 @@ const StyledHeader = styled.section`
     ul{
       display: grid;
       grid-template-columns: repeat(3, 1fr);
+      
+      li{
+        color: #64EB15;
+      }
     }
   }
 `
