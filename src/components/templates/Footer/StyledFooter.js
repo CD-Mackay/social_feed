50b0fd47a61dcf249dfbfc9a84b71f54ae782454
Connor Mackay <connor.mackay@gmail.com>
2021-07-16 18:06:21 -0400

    Create footer component

---
+++ src/components/templates/Footer/StyledFooter.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+const StyledFooter = styled.section`
+  background-color: #6E0A70;
+  bottom: 0;
+  height: 100px;
+  position: fixed;
+  width: 100%;
+`
+
+export default StyledFooter;
