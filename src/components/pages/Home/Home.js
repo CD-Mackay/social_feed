195158cc454f0b47a34116799e8e89b036d855f3
Connor Mackay <connor.mackay@gmail.com>
2021-07-16 18:33:55 -0400

    style social card

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -11,7 +11,29 @@ const Home = ({ data }) => {
   return (
     <StyledHome>
       <section className="center-column">
-
+        <article className="social-card">
+          <div className="img-wrapper">
+          </div>
+          <section className="post-content">
+            <div className="author-date">
+              <p>{data[0].author} - {data[0].date}</p>
+            </div>
+              <h3>{data[0].title}</h3>
+              <p>{data[0].content}</p>
+          </section>
+          <section className="post-control">
+            <div className="vote">
+              <p>Upvote</p>
+              <p>Downvote</p>
+              <p>Retweeeeet</p>
+            </div>
+            <div className="share">
+              <p>Insta</p>
+              <p>Facebook</p>
+              <p>Tiktok</p>
+            </div>
+          </section>
+        </article>
       </section>
     </StyledHome>
   )
--- src/components/pages/Home/StyledHome.js
+++ src/components/pages/Home/StyledHome.js
@@ -9,6 +9,20 @@ const StyledHome = styled.section`
     height: 80vh;
     margin: 0 auto;
   }
+
+  .social-card {
+
+    .post-control {
+      display: grid;
+      grid-template-columns: 40% 40%;
+      justify-content: space-between;
+      
+      .vote, .share {
+        display: grid;
+        grid-template-columns: repeat(3, 1fr);
+      }
+    }
+  }
 `
 
 export default StyledHome;
