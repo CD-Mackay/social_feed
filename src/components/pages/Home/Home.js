77b1cb43d06070c2f0a684ddcdcb180303af0504
Connor Mackay <connor.mackay@gmail.com>
2021-07-17 16:09:14 -0400

    add icons to homepage

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -6,10 +6,16 @@ import React from 'react';
 
 /* Asset Imports */
 import StyledHome from './StyledHome';
+import { FaArrowAltCircleUp, 
+         FaArrowCircleDown, 
+         FaRetweet,
+         FaInstagram,
+         FaFacebookSquare,
+         FaDigg } from "react-icons/fa";
+
 
 const Home = ({ data }) => {
 
-  console.log(data[0])
 
   return (
     <StyledHome socialBG={data[0].img}>
@@ -26,14 +32,14 @@ const Home = ({ data }) => {
           </section>
           <section className="post-control">
             <div className="vote">
-              <p>Upvote - {data[0].likes}</p>
-              <p>Downvote - {data[0].dislike}</p>
-              <p>Retweeeeet</p>
+              <p><FaArrowAltCircleUp /> - {data[0].likes}</p>
+              <p><FaArrowCircleDown /> - {data[0].dislike}</p>
+              <p><FaRetweet /></p>
             </div>
             <div className="share">
-              <p>Insta</p>
-              <p>Facebook</p>
-              <p>Tiktok</p>
+              <FaInstagram />
+              <FaFacebookSquare />
+              <FaDigg />
             </div>
           </section>
         </article>
