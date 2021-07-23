5a13f3c426a81f048f50e13a1076b90a25615fea
Connor Mackay <connor.mackay@gmail.com>
2021-07-23 17:06:52 -0400

    add breakpoints to center column

--- src/components/pages/Home/StyledHome.js
+++ src/components/pages/Home/StyledHome.js
@@ -5,9 +5,21 @@ const StyledHome = styled.section`
   height:80vh;
 
   .center-column{
-    width: 55%;
+    width: 95%;
     padding: 85px 10px 85px 10px;
     margin: 0 auto;
+  }
+
+  @media screen and (min-width: 576px) {
+    .center-column {
+      width: 75%;
+    }
+  }
+
+  @media screen and (min-width: 768px) {
+    .center-column {
+      width: 55%;
+    }
   }
 `
 
